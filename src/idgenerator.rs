fb0e130e424d49677318f7effc3f29fb78f25a4f
corofides <jamlendrem@gmail.com>
2026-03-03 07:53:10 +0000

    Add unit tests for id generator

--- src/idgenerator.rs
+++ src/idgenerator.rs
@@ -14,3 +14,32 @@ impl IdGenerator {
         current_id
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_single_type() {
+        let mut id_generator = IdGenerator::default();
+
+        let first = id_generator.get_new_id(PressTypes::Post);
+        assert_eq!(first, 0);
+
+        let second = id_generator.get_new_id(PressTypes::Post);
+        assert_eq!(second, 1);
+    }
+
+    #[test]
+    fn check_multi_type() {
+        let mut id_generator = IdGenerator::default();
+
+        let mut posts = id_generator.get_new_id(PressTypes::Post);
+        posts = id_generator.get_new_id(PressTypes::Post);
+        let mut users = id_generator.get_new_id(PressTypes::User);
+        posts = id_generator.get_new_id(PressTypes::Post);
+
+        assert_eq!(posts, 2);
+        assert_eq!(users, 0);
+    }
+}
