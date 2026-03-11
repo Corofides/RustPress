3c606cb395ea7d85aaf17982b13f1795c4960d4c
corofides <jamlendrem@gmail.com>
2026-03-11 21:07:42 +0000

    Add a unit test to the service;

--- src/service/post_service.rs
+++ src/service/post_service.rs
@@ -20,3 +20,34 @@ impl<T: PostRepository> PostService<T> {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use sqlx::Error;
+
+    struct MockRepo;
+
+    impl PostRepository for MockRepo {
+        fn fetch_all(&self) -> impl Future<Output = Vec<Post>> {
+            async {
+                vec![]
+            }
+        }
+        fn create_post(&self, post: &Post) -> impl Future<Output = Result<(), Error>> {
+            async {
+                Ok(())
+            }
+        }
+    }
+
+    fn test_get_posts() {
+        let mock = MockRepo;
+        let post_service = PostService::new(mock);
+
+        let posts = post_service.get_posts();
+
+        assert!(false);
+    }
+}
