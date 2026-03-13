59fc589c4d27cfaf650bb8fc388125b8d011e9dc
corofides <jamlendrem@gmail.com>
2026-03-13 07:08:56 +0000

    Add PostMeta Repository;

--- src/repository.rs
+++ src/repository.rs
@@ -1,8 +1,10 @@
 pub mod post_repository;
 pub mod user_repository;
+pub mod postmeta_repository;
 
 pub use user_repository::UserFilters;
 pub use post_repository::PostFilters;
+pub use postmeta_repository::PostmetaFilters;
 
 pub enum RepositoryError {
     AddItemError
---
+++ src/repository/postmeta_repository.rs
@@ -0,0 +1,86 @@
+use sqlx::{
+    Sqlite,
+    QueryBuilder,
+    SqlitePool
+};
+use super::{
+    Repository,
+    RepositoryError,
+};
+use crate::structs::postmeta::PostMeta;
+
+pub struct PostmetaFilters {}
+
+pub struct SqlitePostmetaRepository<'a> {
+    pool: &'a SqlitePool
+}
+
+impl<'a> SqlitePostmetaRepository<'a> {
+    pub fn new(pool: &'a SqlitePool) -> Self {
+        Self {
+            pool
+        }
+    }
+}
+
+impl Repository<PostMeta, PostmetaFilters> for SqlitePostmetaRepository<'_> {
+    async fn add(&self, item: PostMeta) -> Result<(), RepositoryError> {
+        let _ = sqlx::query("INSERT INTO postmeta (
+                    post, key, value 
+                ) VALUES (
+                    ?,
+                    ?,
+                    ?
+                )
+            ")
+            .bind(item.post())
+            .bind(item.key())
+            .bind(item.value())
+            .execute(self.pool)
+            .await;
+
+        Ok(())
+    }
+    async fn fetch(&self, id: u32) -> Option<PostMeta> {
+        let post_meta = sqlx::query_as::<_, PostMeta>("
+                SELECT id, post, key, value FROM post_meta WHERE id = ?
+            ")
+            .bind(id)
+            .fetch_one(self.pool)
+            .await
+            .unwrap();
+
+        return Some(post_meta);
+    }
+    async fn fetch_all(&self) -> Vec<PostMeta> {
+        let mut query_builder: QueryBuilder<Sqlite> = QueryBuilder::new("
+            SELECT id, post, key, value 
+            FROM post_meta 
+            WHERE 1=1
+        ");
+
+        let query = query_builder.build_query_as::<PostMeta>();
+        let meta: Vec<PostMeta> = query
+            .fetch_all(self.pool)
+            .await
+            .unwrap();
+
+        return meta;
+    }
+    async fn fetch_filtered(&self, filters: PostmetaFilters) -> Vec<PostMeta> {
+        let mut query_builder: QueryBuilder<Sqlite> = QueryBuilder::new("
+            SELECT id, post, key, value 
+            FROM post_meta 
+            WHERE 1=1
+        ");
+
+        let query = query_builder.build_query_as::<PostMeta>();
+        let meta: Vec<PostMeta> = query
+            .fetch_all(self.pool)
+            .await
+            .unwrap();
+
+        return meta;
+
+    }
+}
