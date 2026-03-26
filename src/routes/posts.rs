8b45892b47439c9c14859a79a7380a98a5abe59d
corofides <jamlendrem@gmail.com>
2026-03-26 19:03:00 +0000

    Return a message by using a custom enum;

--- src/routes/posts.rs
+++ src/routes/posts.rs
@@ -13,6 +13,10 @@ use crate::State;
 use crate::AppState;
 use async_std::sync::Arc;
 use http::StatusCode;
+use axum::response::{
+    Response,
+    IntoResponse,
+};
 
 pub fn get_routes() -> Vec<(String, String, MethodRouter<Arc<AppState>>)> {
     vec![
@@ -48,8 +52,25 @@ async fn add_post(State(state): State<Arc<AppState>>, Json(payload): Json<Post>)
 
 }
 
+enum RequestError {
+    NotFound,
+}
+
+impl IntoResponse for RequestError {
+    fn into_response(self) -> Response {
+        let (status, message) = match self {
+            Self::NotFound => {
+                (StatusCode::NOT_FOUND, "Post Not Found")
+            }
+        };
+
+        (status, Json(json!({ "error": message }))).into_response()
+
+    }
+}
+
 #[axum::debug_handler]
-async fn get_post(State(state): State<Arc<AppState>>, Path(payload): Path<u32>) -> Result<Json<Post>, StatusCode> {
+async fn get_post(State(state): State<Arc<AppState>>, Path(payload): Path<u32>) -> Result<Json<Post>, RequestError> {
 
     let service_provider = state.service_provider.lock().await;
     let post_service = service_provider.post_service();
@@ -57,7 +78,7 @@ async fn get_post(State(state): State<Arc<AppState>>, Path(payload): Path<u32>) 
     let post = post_service.get_post(payload);
 
     let Some(post) = post else {
-        return Err(StatusCode::NOT_FOUND);
+        return Err(RequestError::NotFound);
     };
 
     Ok(Json(
